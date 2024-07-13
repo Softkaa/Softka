8cf717bb95a4ccbfdc674fa4d3784958e68237b0
Fernando <fjgt2000@gmail.com>
2024-07-13 15:49:50 -0500

    build(dev-exceptions): se crea capa manejo de excepciones

---
+++ Utils/Exceptions/ErrorExceptions.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Softka.Utils.Exceptions
+{
+    public class ErrorExceptions
+    {
+        //Not found
+        public static ProblemDetails CreateNotFound()
+        {
+            return new ProblemDetails
+            {
+                Title = "Not found",
+                Status = StatusCodes.Status404NotFound,
+                Detail = "The request has not been found"
+            };
+        }
+
+        //Server error 500
+        public static ProblemDetails CreateServerError()
+        {
+            return new ProblemDetails
+            {
+                Title = "Server error",
+                Status = StatusCodes.Status500InternalServerError,
+                Detail = "An error has occurred while processing your request"
+            };
+        }
+
+        //Badrequest
+        public static ProblemDetails CreateBadRequest()
+        {
+            return new ProblemDetails
+            {
+                Title = "Bad Request",
+                Status = StatusCodes.Status500InternalServerError,
+                Detail = "Bad Request. Please check the submitted data and try again."
+            };
+        }
+    }
+}
