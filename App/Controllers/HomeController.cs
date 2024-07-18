66aa6b8f8a5c03a21ffb571b895fdfe21a11f228
Mateo020528 <mateo.velez.censa@gmail.com>
2024-07-17 19:53:46 -0500

    fix(interface): se modifica Home controller

--- App/Controllers/HomeController.cs
+++ App/Controllers/HomeController.cs
@@ -5,92 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Softka.Utils;
 
-using DinkToPdf;
-using DinkToPdf.Contracts;
-using Microsoft.AspNetCore.Http.Extensions;
-using Softka.Infrastructure.Data;
-using Softka.Models;
-
 namespace Softka.App.Controllers
 {
-    /* [AuthorizationRequired] */
+//     [AuthorizationRequired]
     public class HomeController : Controller
     {
-        private readonly IConverter _converter;
-        private readonly BaseContext _context;
-        public HomeController(IConverter converter, BaseContext context)
-        {
-            _converter = converter;
-            _context = context;
-        }
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult ViewToPdf()
-        {
-            return View();
-        }
-
-        public IActionResult ShowPdfOnPage()
-        {
-            string ActualPage = HttpContext.Request.Path;
-            string UrlPage = HttpContext.Request.GetEncodedUrl();
-            UrlPage = UrlPage.Replace(ActualPage, "");
-            UrlPage = $"{UrlPage}/Home/ViewToPdf";
-
-
-            var Pdf = new HtmlToPdfDocument()
-            {
-                GlobalSettings = new GlobalSettings()
-                {
-                    PaperSize = PaperKind.A4,
-                    Orientation = Orientation.Portrait
-                },
-                Objects = {
-                    new ObjectSettings()
-                    {
-                        Page = UrlPage
-                    }
-                }
-            };
-
-            var ArchivePdf = _converter.Convert(Pdf);
-
-
-            return File(ArchivePdf, "application/pdf");
-        }
-
-        public IActionResult DownloadPdf(User user)
-        {
-            string ActualPage = HttpContext.Request.Path;
-            string UrlPage = HttpContext.Request.GetEncodedUrl();
-            UrlPage = UrlPage.Replace(ActualPage, "");
-            UrlPage = $"{UrlPage}/Home/ViewToPdf";
-
-
-            var Pdf = new HtmlToPdfDocument()
-            {
-                GlobalSettings = new GlobalSettings()
-                {
-                    PaperSize = PaperKind.A4,
-                    Orientation = Orientation.Portrait
-                },
-                Objects = {
-                    new ObjectSettings()
-                    {
-                        Page = UrlPage
-                    }
-                }
-            };
-
-            var ArchivePdf = _converter.Convert(Pdf);
-            var UserName = user.Names;
-            string PdfName = $"{UserName}_" + DateTime.Now.ToString("ddMMyyyyHHmmss") + ".pdf";
-
-
-            return File(ArchivePdf, "application/pdf", PdfName);
         }
     }
 }
